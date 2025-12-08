ae78b6434ac4ecc8fa0d838be3afa42f834c9775
jfct <jorgecosteixeira@gmail.com>
2025-12-08 22:51:39 +0000

    fix: removed the musicbrainz-api client, turns out its not compatible, project is commonjs

--- src/clients/tracklist/adapters/tracklist-adapter.factory.ts
+++ src/clients/tracklist/adapters/tracklist-adapter.factory.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ITracklistAdapter } from '../interfaces/tracklist-adapter.interface';
-import { MusicBrainzAdapter } from './musicbrainz.adapter';
 import { AdapterType } from '../enums/adapter-type.enum';
 import { HttpMusicBrainzAdapter } from './http-musicbrainz.adapter';
 
 @Injectable()
 export class TracklistAdapterFactory {
   constructor(
-    private readonly musicBrainzAdapter: MusicBrainzAdapter,
     private readonly httpMusicBrainzAdapter: HttpMusicBrainzAdapter,
   ) {}
 
   getAdapter(adapterType: AdapterType): ITracklistAdapter {
     switch (adapterType) {
-      case AdapterType.MUSICBRAINZ_CLIENT:
-        return this.musicBrainzAdapter;
-
       case AdapterType.HTTP_MUSICBRAINZ:
         return this.httpMusicBrainzAdapter;
 
--- src/clients/tracklist/enums/adapter-type.enum.ts
+++ src/clients/tracklist/enums/adapter-type.enum.ts
@@ -1,4 +1,3 @@
 export enum AdapterType {
-  MUSICBRAINZ_CLIENT = 'musicbrainz-client',
   HTTP_MUSICBRAINZ = 'http-musicbrainz',
 }
